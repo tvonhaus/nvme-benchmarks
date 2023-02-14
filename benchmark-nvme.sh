b35b2ad8f4f213e230f10aed2b2afa21a8a0448e
Taylor von Hausen <tvonhausen@gmail.com>
2023-02-14 15:14:36 -0500

    Updated benchmark-nvme.sh
    Removed xterm utilization, fixed output to txt

--- benchmark-nvme.sh
+++ benchmark-nvme.sh
@@ -2,12 +2,15 @@
 DISK='nvme0n1'
 
 # write test
-dd if=/dev/zero of=/dev/$DISK status=progress oflag=direct bs=10M count=1000 >> write-speed.txt
+echo 'Testing Write...'
+dd if=/dev/zero of=/dev/$DISK status=progress oflag=direct bs=10M count=1000 2> write-speed.txt
 # read test
-dd of=/dev/null if=/dev/$DISK status=progress iflag=direct bs=10M count=1000 >> read-speed.txt
+echo 'Testing Read...'
+dd of=/dev/null if=/dev/$DISK status=progress iflag=direct bs=10M count=1000 2> read-speed.txt
 
 #Bidirectional tests, xterm used to run at same time
+echo 'Testing Bidirectional...'
 # bi-write test
-xterm -e dd if=/dev/zero of=/dev/$DISK status=progress oflag=direct bs=10M count=1000 >> bidirectional-write-speed.txt &
+dd if=/dev/zero of=/dev/$DISK status=progress oflag=direct bs=10M count=1000 2> bidirectional-write-speed.txt &
 # bi-read test
-xterm -e dd of=/dev/null if=/dev/$DISK status=progress iflag=direct bs=10M count=1000 >> bidirectional-read-speed.txt
+dd of=/dev/null if=/dev/$DISK status=progress iflag=direct bs=10M count=1000 2> bidirectional-read-speed.txt
